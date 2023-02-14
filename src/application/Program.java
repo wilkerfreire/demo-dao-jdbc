18d2a7a16bdeed2f1ee8395917f79ca5b5af51a9
Wilker Oliveira Freire <wilkerf8@gmail.com>
2023-02-14 20:07:24 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		
 		System.out.println(obj);
 		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		System.out.println(seller);
 	}
 
 }
