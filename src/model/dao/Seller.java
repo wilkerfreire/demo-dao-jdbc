a26623b15bd55038ec9e9346f63b66b30fc0cb5c
Wilker Oliveira Freire <wilkerf8@gmail.com>
2023-02-14 20:15:40 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/Seller.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import java.util.List;
+
+public interface Seller {
+
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
