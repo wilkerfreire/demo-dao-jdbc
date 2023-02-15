2ee10110c107314c40653777d52fd60c4f0bc135
Wilker Oliveira Freire <wilkerf8@gmail.com>
2023-02-15 16:50:00 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, new Department(2, null));
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
