354c346ca141497b91a8a317699eae14b64f8af9
Wilker Oliveira Freire <wilkerf8@gmail.com>
2023-02-15 16:26:29 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -25,6 +27,11 @@ public class Program {
 		list = sellerDao.findAll();
 		for(Seller s : list)
 			System.out.println(s);
+		
+		System.out.println("\n===== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, new Department(2, null));
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
